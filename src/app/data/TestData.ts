4ee10b2f788b715f211cee610bf3ce152f8e89c6
Oleg Khilko <khilkoleg@gmail.com>
2023-01-19 14:13:56 +0300

    feature: add new testdata

--- src/app/data/TestData.ts
+++ src/app/data/TestData.ts
@@ -17,43 +17,107 @@ export class TestData {
   ];
 
   static priorities: Priority[] = [
-    {id: 1, title: 'Low', color: '#E5E5E5'},
-    {id: 2, title: 'Medium', color: '#85D1B2'},
-    {id: 3, title: 'High', color: '#F1828D'},
+    {id: 1, title: 'Low'},
+    {id: 2, title: 'Medium'},
+    {id: 3, title: 'High'},
   ];
 
   static tasks: Task[] = [
     {
       id: 1,
-      title: 'Залить бензин полный бак',
+      title: 'Wash my car',
       priority: TestData.priorities[0],
-      completed: false,
+      completed: true,
       category: TestData.categories[9],
-      date: new Date('2022-01-18')
+      date: new Date('2023-01-18')
     },
     {
       id: 2,
-      title: 'Залить бензин полный бак',
+      title: 'Take a pill',
       priority: TestData.priorities[1],
       completed: false,
       category: TestData.categories[8],
-      date: new Date('2022-01-18')
+      date: new Date('2023-01-19')
     },
     {
       id: 3,
-      title: 'Залить бензин полный бак',
+      title: 'Buy a new phone',
       completed: false,
       category: TestData.categories[7],
-      date: new Date('2022-01-18')
+      date: new Date('2023-01-15')
     },
     {
       id: 4,
-      title: 'Залить бензин полный бак',
-      priority: TestData.priorities[3],
+      title: 'Transfer 10% from salary',
+      priority: TestData.priorities[1],
       completed: true,
       category: TestData.categories[6],
-      date: new Date('2022-01-18')
-    }
+      date: new Date('2023-01-10')
+    },
+    {
+      id: 5,
+      title: 'Buy a fresh bread',
+      priority: TestData.priorities[0],
+      completed: false,
+      category: TestData.categories[5],
+      date: new Date('2023-01-11')
+    },
+    {
+      id: 6,
+      title: 'Go to the gym',
+      priority: TestData.priorities[0],
+      completed: true,
+      category: TestData.categories[4],
+      date: new Date('2023-01-12')
+    },
+    {
+      id: 7,
+      title: 'Draft an agreement',
+      priority: TestData.priorities[1],
+      completed: false,
+      category: TestData.categories[6],
+      date: new Date('2023-01-13')
+    },
+    {
+      id: 8,
+      title: 'Go for a walk with friends',
+      priority: TestData.priorities[2],
+      completed: true,
+      category: TestData.categories[3],
+      date: new Date('2023-01-11')
+    },
+    {
+      id: 9,
+      title: 'Learn rxjs',
+      priority: TestData.priorities[2],
+      completed: false,
+      category: TestData.categories[2],
+      date: new Date('2023-01-17')
+    },
+    {
+      id: 10,
+      title: 'Buy a present',
+      priority: TestData.priorities[0],
+      completed: false,
+      category: TestData.categories[1],
+      date: new Date('2023-01-19')
+    },
+    {
+      id: 11,
+      title: 'Track expenses',
+      priority: TestData.priorities[1],
+      completed: true,
+      category: TestData.categories[6],
+      date: new Date('2023-01-02')
+    },
+    {
+      id: 12,
+      title: 'Buy water',
+      priority: TestData.priorities[2],
+      completed: false,
+      category: TestData.categories[5],
+      date: new Date('2023-01-01')
+    },
   ]
 
 }
--- src/app/model/Priority.ts
+++ src/app/model/Priority.ts
@@ -1,11 +1,9 @@
 export class Priority {
   id: number;
   title: string;
-  color: string;
 
-  constructor(id: number, title: string, color: string) {
+  constructor(id: number, title: string) {
     this.id = id;
     this.title = title;
-    this.color = color;
   }
 }
